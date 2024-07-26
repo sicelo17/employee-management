60804f56a7d09387822995da3abc5fd32dca7b41
Sicelo Sitsha <70627871+sicelo17@users.noreply.github.com>
2024-07-26 22:31:34 +0200

    add view all employees

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://144.91.111.46:8000/api/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
